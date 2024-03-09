1d2c0211602edb8b5848e3e166d4ebbcaeaa5b28
josleodal <josleodal@gmail.com>
2024-03-09 12:07:17 +0100

    22

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-// index.js
 const express = require('express');
 const dotenv = require('dotenv');
 const { dbConnection } = require('./config/db');
@@ -6,37 +5,36 @@ const path = require('path');
 const productRoutes = require('./routes/productRoutes');
 const cors = require('cors');
 
-
-
-
-
 dotenv.config();
 
 const app = express();
 const port = 8082;
 
 app.use(cors());
+
+// Middleware para el registro de solicitudes
 app.use((req, res, next) => {
   console.log(`Request received: ${req.method} ${req.url}`);
   next();
 });
 
-
 app.use(express.urlencoded({ extended: true }));
 
-//app.use('/uploads', express.static(path.join(__dirname, '../public/uploads')));
+// Middleware para servir archivos estáticos en la ruta /uploads
+app.use('/uploads', express.static(path.join(__dirname, '../public/uploads')));
+
+// Middleware para servir archivos estáticos en la carpeta ../public
 app.use(express.static(path.join(__dirname, '../public')));
 
+// Middleware para analizar solicitudes JSON (descomenta si es necesario)
+// app.use(express.json());
 
+// Rutas definidas en productRoutes
+app.use('/', productRoutes);
 
-
-app.use(express.json()); 
-app.use('/', productRoutes); 
-
+// Conexión a la base de datos
 dbConnection();
 
 app.listen(port, () => {
   console.log(`Server started on port http://localhost:${port}/products`);
 });
-
-
