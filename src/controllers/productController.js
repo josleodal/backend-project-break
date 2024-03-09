ad76b82a3b44379e037705f5a627e40bde78b82d
josleodal <josleodal@gmail.com>
2024-03-09 15:33:30 +0100

    ultima 999to

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -7,13 +7,14 @@ const path = require('path');
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        cb(null, path.join(__dirname, '../public/uploads'));
+        const destinationPath = path.join(__dirname, '../public/uploads');
+        console.log('Destination Path:', destinationPath);
+        cb(null, destinationPath);
     },
     filename: function (req, file, cb) {
         cb(null, Date.now() + '-' + file.originalname);
     },
 });
-
 
   
   const upload = multer({ storage: storage });
